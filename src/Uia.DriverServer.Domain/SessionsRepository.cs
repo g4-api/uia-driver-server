62e28991e5c8e3d94d695e58e9cf7f7e80d8e936
gravity-api <gravity.api@outlook.com>
2024-06-11 15:53:01 +0300

    Removed System.Linq from SessionsRepository.cs
    Removed the `System.Linq` namespace from the `SessionsRepository.cs` file, indicating that LINQ queries or methods are no longer being used in this file.

--- src/Uia.DriverServer.Domain/SessionsRepository.cs
+++ src/Uia.DriverServer.Domain/SessionsRepository.cs
@@ -11,7 +11,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
-using System.Linq;
 using System.Text.Json;
 using System.Xml.Linq;
 
